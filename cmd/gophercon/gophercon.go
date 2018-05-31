08e85c729d2260c0b834ad090a4c0529123bed03
Jenna Riseley <jf.riseley@gmail.com>
2018-05-31 11:16:24 +0000

    Addd port configuration

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -4,13 +4,18 @@ import (
 	"log"
 	"net/http"
 	"github.com/jriseley/gophercon18/pkg/routing"
+	"os"
 )
 
 
 func main() {
 	log.Printf("Service is starting...")
+	port := os.Getenv("SERVICE_PORT")
+	if len(port) == 0 {
+		log.Fatal("Service port wasn't set\n")
+	}
 	r := routing.BaseRouter()
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
 
