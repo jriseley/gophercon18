86d0c6adcb834086768668266324493f825fd7ba
Jenna Riseley <jf.riseley@gmail.com>
2018-05-31 11:08:13 +0000

    Added a test

---
+++ pkg/routing/routing_test.go
@@ -0,0 +1,30 @@
+package routing_test
+
+import (
+
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"github.com/jriseley/gophercon18/pkg/routing"
+
+)
+
+func TestBaseRouter(t *testing.T){
+	handler := routing.BaseRouter()
+
+	ts := httptest.NewServer(handler)
+
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/home")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Wrong status code %d (%d expected", res.StatusCode, http.StatusOK)
+	}
+  
+}
